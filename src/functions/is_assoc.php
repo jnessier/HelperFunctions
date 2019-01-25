bf3409085406b16035c80a565a79433f30eb2c92
rjgamer <jonathan.nessier@neoflow.ch>
2019-01-25 13:41:06 +0100

    Fixed compare

--- src/functions/is_assoc.php
+++ src/functions/is_assoc.php
@@ -3,18 +3,14 @@
 /**
  * Check whether an empty or associative array.
  *
- * @param mixed $array Array
+ * @param mixed $array Array to check
  *
  * @return bool
  */
 function is_assoc($array): bool
 {
     if (is_array($array)) {
-        if (count($array) > 0) {
-            return count(array_filter(array_keys($array), 'is_string')) > count($array);
-        }
-
-        return true;
+        return count(array_filter(array_keys($array), 'is_string')) === count($array);
     }
 
     return false;

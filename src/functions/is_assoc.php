ebeb6ea3a5637a89ad99b6700c9850fc8c5b164f
rjgamer <jonathan.nessier@neoflow.ch>
2019-01-25 13:30:38 +0100

    Fixed statement

--- src/functions/is_assoc.php
+++ src/functions/is_assoc.php
@@ -11,7 +11,7 @@ function is_assoc($array): bool
 {
     if (is_array($array)) {
         if (count($array) > 0) {
-            return count(array_filter(array_keys($array), 'is_string')) > 0;
+            return count(array_filter(array_keys($array), 'is_string')) > count($array);
         }
 
         return true;

ce9f3ade84ecd2aa20569f069435d620c4aa67a2
rjgamer <jonathan.nessier@neoflow.ch>
2019-02-15 10:41:07 +0100

    Added trim to sanitize file names

--- src/functions/sanitize_file_name.php
+++ src/functions/sanitize_file_name.php
@@ -5,14 +5,14 @@
  *
  * @see https://stackoverflow.com/a/2021729/2338829
  *
- * @param string $name       Name of file (or foldeR)
- * @param bool   $hiddenFile Set FALSE to prevent hidde file name (UNIX only)
+ * @param string $name       Name of file (or folder)
+ * @param bool   $hiddenFile Set FALSE to prevent hide file name (UNIX only)
  *
  * @return string
  */
 function sanitize_file_name(string $name, bool $hiddenFile = true): string
 {
-    $name = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', $name);
+    $name = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', trim($name));
     if (!$hiddenFile) {
         $name = ltrim($name, '.');
     }
